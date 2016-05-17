3f4e3d7b65deb0baa1392231f3a98725c6ab3943
macbook <592944119@qq.com>
2016-05-17 16:33:08 +0800

    提交文件

---
+++ 高德地图/高德地图/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  高德地图
+//
+//  Created by 赵宏亚 on 16/5/17.
+//  Copyright © 2016年 赵宏亚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
